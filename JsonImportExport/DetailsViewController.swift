0fb05e32fc85d17845172ac1f6d8732c27770109
xinpanda321321 <56233565+xinpanda321321@users.noreply.github.com>
2021-05-09 16:31:07 -0700

    Modify result2
    modify result 2

--- JsonImportExport/DetailsViewController.swift
+++ JsonImportExport/DetailsViewController.swift
@@ -352,19 +352,19 @@ extension DetailsViewController: UITextFieldDelegate, UITextViewDelegate {
             else if textField.tag == 29 {
                 note.becomeFirstResponder()
                 var temp = ""
-                temp += "SC: " + sc.text! + "   Enter\n"
-                temp += "well: " + well.text! + "   Enter\n"
+                temp += "SC: " + sc.text! + " \"Enter\"\n"
+                temp += "well: " + well.text! + " \"Enter\"\n"
                 var tempLabel1 = ""
                 for i in 0..<18 {
                     let cell = collectionView1.cellForItem(at: IndexPath(row: i, section: 0)) as! CollectionViewCell1
-                    tempLabel1 +=  cell.cv1TF.text! + " Enter "
+                    tempLabel1 +=  cell.cv1TF.text! + " \"Enter\"\n"
                 }
                 temp += tempLabel1 + "\n"
                 
                 var tempLabel2 = ""
                 for i in 0..<6 {
                     let cell = collectionView2.cellForItem(at: IndexPath(row: i, section: 0)) as! CollectionViewCell2
-                    tempLabel2 += cell.cl2TF.text! + " Enter "
+                    tempLabel2 += cell.cl2TF.text! + " \"Enter\"\n"
                 }
                 temp += tempLabel2 + "\n"
                 note.text = temp
